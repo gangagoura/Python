652323a21095448ca5d17205c4ef40a467d7d62d
GUILHERME FERNANDES NETO <40431403+gangagoura@users.noreply.github.com>
2021-07-26 23:21:35 -0300

    Create FolhaPagamento.py
    desafio digital innovation one python

---
+++ Python/Python/Solucionando Desafios em Python/FolhaPagamento.py
@@ -0,0 +1,7 @@
+emp_no = int(input())
+worked_hours = int(input())
+receives_per_worked_hour = float(input())
+salary = receives_per_worked_hour * worked_hours
+
+print(f"NUMBER = {emp_no}")
+print(f"SALARY = U$ {salary:.2f}")
