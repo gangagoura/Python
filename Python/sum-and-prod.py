401b944b3fd7949f92de8582803e55d087783170
GUILHERME FERNANDES NETO <40431403+gangagoura@users.noreply.github.com>
2022-11-26 13:57:59 -0300

    Create sum-and-prod.py

---
+++ Python/sum-and-prod.py
@@ -0,0 +1,5 @@
+import numpy
+numpy.set_printoptions(legacy="1.13")
+N, M = map(int, input().split())
+A = numpy.array([input().split() for _ in range(N)], int)
+print(numpy.prod(numpy.sum(A, axis=0), axis=0))
